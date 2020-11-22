bf998d886b9210272879d967463bad3f9eb562ec
Tu Houwang <tuhouwang96@163.com>
2020-11-22 13:02:21 +0800

    更新了若干函数的命名，使其更符合国际风格

--- NM_CT.m
+++ NM_CT.m
@@ -33,8 +33,8 @@ clc;
 tic;
 
 [casename,Nw,Nb,cpmax,dr,zs,zr,rmax,freq,interface,bottom,dz,...
-    Lowerboundary,tlmin,tlmax,rhow,rhob,alphaw,alphab,depw,cw,...
-    depb,cb] = ReadEnvParameter('input.txt');
+    Lowerboundary,tlmin,tlmax,depw,cw,rhow,alphaw,...
+    depb,cb,rhob,alphab] = ReadEnvParameter('input.txt');
 
 cw     = Interpolation(depw,cw,Nw,0,interface);
 cb     = Interpolation(depb,cb,Nb,interface,bottom);
@@ -44,32 +44,31 @@ alphaw = Interpolation(depw,alphaw,Nw,0,interface);
 alphab = Interpolation(depb,alphab,Nb,interface,bottom);
 
 %----------------------------------------------
-
-[nr,r,rho_zs,kw,kb] = Initialization(Nw,Nb,freq,rmax,dr,zs,...
+[nr,r,rhozs,kw,kb,w] = Initialization(Nw,Nb,freq,rmax,dr,zs,...
     rhow,rhob,cw,cb,alphaw,alphab,interface,bottom);
 
-[kr,eigvector_w,eigvector_b] = Eigen_value_vector(Nw,Nb,interface,...
+[kr,eigvectorw,eigvectorb] = EigenValueVector(Nw,Nb,interface,...
     bottom,kw,kb,rhow,rhob,Lowerboundary);
 
-[n_modes,kr,eigvector_w,eigvector_b] = Modes_number(freq,kr,...
-    eigvector_w,eigvector_b,cpmax);
+[nmodes,kr,eigvectorw,eigvectorb] = NumofModes(w,kr,...
+    eigvectorw,eigvectorb,cpmax);
 
-[psi,z] = Generate_modes(eigvector_w,eigvector_b,n_modes,dz,...
+[psi,z] = GenerateModes(eigvectorw,eigvectorb,nmodes,dz,...
     rhow,rhob,interface,bottom);
 
-[tl,tl_zr] = Calculate_tl_field(n_modes,nr,r,kr,zs,rho_zs,dz,psi,zr);
+[tl,tl_zr] = SynthesizeSoundField(nmodes,nr,r,kr,zs,rhozs,dz,psi,zr);
 
 %---------------print the results-------------------
-Print_wavenumbers(kr,casename);
-% Print_mode(psi,z);
-Print_tl_zr(r,zr,tl_zr);
-Print_tl_field(r,z,tl,tlmin,tlmax,casename,interface);
-% Write_to_file('tl.bin',tlmin,tlmax,r,z,tl);
+ShowWavenumbers(kr,casename);
+% ShowMode(psi,z);
+ShowTLcurve(r,zr,tl_zr);
+ShowSoundField(r,z,tl,tlmin,tlmax,casename,interface);
+% SaveSoundField('tl.bin',tlmin,tlmax,r,z,tl);
 toc;
 %--------------------------------------------------------------------------
 function [casename,Nw,Nb,cpmax,dr,zs,zr,rmax,freq,interface,...
-    bottom,dz,Lowerboundary,tlmin,tlmax,rhow,rhob,...
-    alphaw,alphab,depw,cw,depb,cb] = ReadEnvParameter(env_file)
+    bottom,dz,Lowerboundary,tlmin,tlmax,depw,cw,rhow,alphaw,...
+    depb,cb,rhob,alphab] = ReadEnvParameter(env_file)
 
     fid           = fopen(env_file);
     casename      = fgetl(fid);
@@ -139,21 +138,21 @@ function c = Interpolation(depc,c,N,s,t)
 
 end
 
-function [nr,r,rho_zs,kw,kb] = Initialization(Nw,Nb,freq,rmax,...
+function [nr,r,rhozs,kw,kb,w] = Initialization(Nw,Nb,freq,rmax,...
     dr,zs,rhow,rhob,cw,cb,alphaw,alphab,interface,bottom)
     
     w       = 2 * pi * freq;
     r       = dr:dr:rmax;
-    nr      = length(r);
-
-    x1   = cos((0 : Nw) * pi / Nw)';
-    x2   = cos((0 : Nb) * pi / Nb)';
+    nr      = length(r);    
+    
     if(zs <= interface)
-        z1    = (1.0 - x1) * interface/2;
-        rho_zs=interp1(z1,rhow,zs,'linear');
+        x1     = cos((0 : Nw) * pi / Nw)';
+        z1     = (1.0 - x1) * interface / 2;
+        rhozs  = interp1(z1,rhow,zs,'linear');
     else
-        z1    = (1.0 - x2) * (bottom - interface) / 2 + interface;
-        rho_zs=interp1(z1,rhob,zs,'linear');
+        x2     = cos((0 : Nb) * pi / Nb)'; 
+        z2     = (1.0 - x2) * (bottom - interface) / 2 + interface;
+        rhozs  = interp1(z2,rhob,zs,'linear');
     end
 
     kw = w./cw.*(1.0+1i*alphaw/(40.0*pi*log10(exp(1.0))));
@@ -161,7 +160,7 @@ function [nr,r,rho_zs,kw,kb] = Initialization(Nw,Nb,freq,rmax,...
     
 end
 
-function D  = DeriveMatrix(n)
+function D  = DerivationMatrix(n)
 
     D = zeros(n, n);
     for k = 1 : n
@@ -172,26 +171,7 @@ function D  = DeriveMatrix(n)
         
 end
 
-function T  = ChebPolynomialValue(n,z)
-
-    m = length(z);
-
-    T = zeros(m, n);
-    for k = 0 : n-1
-        T(:, k+1) = cos( k * acos(z) );
-    end
-
-end
-
-function fx = invCheb(fk,z)
-
-    n = size(fk, 1);
-    T = ChebPolynomialValue(n, z);
-    fx = T * fk;
-    
-end
-
-function C  = Convolution(v)
+function C  = ConvolutionMatrix(v)
 
     n = length(v);
     C = zeros(n, n);
@@ -210,10 +190,29 @@ function C  = Convolution(v)
     
 end
 
-function fk = Cheb(fx,z)
+function T  = ChebPolyValue(n,z)
+
+    m = length(z);
+
+    T = zeros(m, n);
+    for k = 0 : n-1
+        T(:, k+1) = cos( k * acos(z) );
+    end
+
+end
+
+function fx = InvChebTrans(fk,z)
+
+    n = size(fk, 1);
+    T = ChebPolyValue(n, z);
+    fx = T * fk;
+    
+end
+
+function fk = ChebTrans(fx,z)
 
     n = length(fx);
-    T = ChebPolynomialValue(n, z);
+    T = ChebPolyValue(n, z);
 
     fx(1) = fx(1) / 2;
     fx(n) = fx(n) / 2;
@@ -225,117 +224,117 @@ function fk = Cheb(fx,z)
     
 end
 
-function [kr,eigvector_w,eigvector_b] = Eigen_value_vector(Nw,Nb,...
+function [kr,eigvectorw,eigvectorb] = EigenValueVector(Nw,Nb,...
     interface,bottom,kw,kb,rhow,rhob,Lowerboundary)
 
-    D1  = DeriveMatrix(Nw+1);
-    D2  = DeriveMatrix(Nb+1);
-    
+    D1  = DerivationMatrix(Nw+1);
+    D2  = DerivationMatrix(Nb+1);    
     x1  = cos( (0 : Nw) * pi / Nw )';
     x2  = cos( (0 : Nb) * pi / Nb )';
    
-    A =4.0 / interface^2 * Convolution(Cheb(rhow, x1)) *...
-        D1 * Convolution(Cheb(1./rhow, x1)) * D1 +...
-        Convolution( Cheb(kw.^2, x1 ) );
-    B =4.0 / (bottom-interface)^2 * Convolution(Cheb(rhob, x2)) *...
-        D2 * Convolution(Cheb(1./rhob, x2)) * D2 +...
-        Convolution( Cheb(kb.^2, x2 ) );
+    A =4.0 / interface^2 * ConvolutionMatrix(ChebTrans(rhow, x1)) *...
+        D1 * ConvolutionMatrix(ChebTrans(1./rhow, x1)) * D1 +...
+        ConvolutionMatrix( ChebTrans(kw.^2, x1 ) );
+    B =4.0 / (bottom-interface)^2 * ConvolutionMatrix(ChebTrans(rhob, x2))...
+        *D2 * ConvolutionMatrix(ChebTrans(1./rhob, x2)) * D2 +...
+        ConvolutionMatrix( ChebTrans(kb.^2, x2 ) );
     
-    ALL=zeros(Nw+Nb+2,Nw+Nb+2);
-    ALL(1:Nw-1,1:Nw-1)              = A(1:Nw-1,1:Nw-1);
-    ALL(1:Nw-1,Nw+Nb-1:Nw+Nb)       = A(1:Nw-1,Nw:Nw+1);
-    ALL(Nw:Nw+Nb-2,Nw:Nw+Nb-2)      = B(1:Nb-1,1:Nb-1);
-    ALL(Nw:Nw+Nb-2,Nw+Nb+1:Nw+Nb+2) = B(1:Nb-1,Nb:Nb+1);
+    U = zeros(Nw+Nb+2,Nw+Nb+2);
+    U(1:Nw-1,1:Nw-1)              = A(1:Nw-1,1:Nw-1);
+    U(1:Nw-1,Nw+Nb-1:Nw+Nb)       = A(1:Nw-1,Nw:Nw+1);
+    U(Nw:Nw+Nb-2,Nw:Nw+Nb-2)      = B(1:Nb-1,1:Nb-1);
+    U(Nw:Nw+Nb-2,Nw+Nb+1:Nw+Nb+2) = B(1:Nb-1,Nb:Nb+1);
     %upper boundary
-    ALL(Nw+Nb-1,1:Nw-1)        = 1.0;
-    ALL(Nw+Nb-1,Nw+Nb-1:Nw+Nb) = 1.0;
+    U(Nw+Nb-1,1:Nw-1)        = 1.0;
+    U(Nw+Nb-1,Nw+Nb-1:Nw+Nb) = 1.0;
     %lower boundary
-    bottom_boundary = (-1.0).^(0:Nb);
+    bottom_boundary = (-1.0) .^ (0:Nb);
     if(Lowerboundary == 1)
         bottom_boundary = bottom_boundary * D2;
     end
-    ALL(Nw+Nb+2,Nw:Nw+Nb-2)      = bottom_boundary(1 :Nb-1);
-    ALL(Nw+Nb+2,Nw+Nb+1:Nw+Nb+2) = bottom_boundary(Nb:Nb+1);
+    U(Nw+Nb+2,Nw:Nw+Nb-2)      = bottom_boundary(1 :Nb-1);
+    U(Nw+Nb+2,Nw+Nb+1:Nw+Nb+2) = bottom_boundary(Nb:Nb+1);
     %first interface boundary
-    ALL(Nw+Nb,1:Nw-1)          = (-1.0).^(0:Nw-2);
-    ALL(Nw+Nb,Nw:Nw+Nb-2)      = -1.0;
-    ALL(Nw+Nb,Nw+Nb-1:Nw+Nb)   = (-1.0).^(Nw-1:Nw);
-    ALL(Nw+Nb,Nw+Nb+1:Nw+Nb+2) = -1.0;
+    U(Nw+Nb,1:Nw-1)          = (-1.0).^(0:Nw-2);
+    U(Nw+Nb,Nw:Nw+Nb-2)      = -1.0;
+    U(Nw+Nb,Nw+Nb-1:Nw+Nb)   = (-1.0).^(Nw-1:Nw);
+    U(Nw+Nb,Nw+Nb+1:Nw+Nb+2) = -1.0;
     %second interface boundary 2
-    Pu=1 / rhow(Nw+1) / interface * ((-1.0) .^ (0:Nw)) * D1;
-    Pd=-1/ rhob(1) / (bottom-interface) * ones(1,Nb+1) * D2;
+    Pu = 1 / rhow(Nw+1) / interface * ((-1.0) .^ (0 : Nw)) * D1;
+    Pd = -1/ rhob(1) / (bottom - interface) * ones(1,Nb+1) * D2;
 
-    ALL(Nw+Nb+1,1:Nw-1)          = Pu(1:Nw-1);
-    ALL(Nw+Nb+1,Nw:Nw+Nb-2)      = Pd(1:Nb-1);
-    ALL(Nw+Nb+1,Nw+Nb-1:Nw+Nb)   = Pu(Nw:Nw+1);
-    ALL(Nw+Nb+1,Nw+Nb+1:Nw+Nb+2) = Pd(Nb:Nb+1);
+    U(Nw+Nb+1,1:Nw-1)          = Pu(1:Nw-1);
+    U(Nw+Nb+1,Nw:Nw+Nb-2)      = Pd(1:Nb-1);
+    U(Nw+Nb+1,Nw+Nb-1:Nw+Nb)   = Pu(Nw:Nw+1);
+    U(Nw+Nb+1,Nw+Nb+1:Nw+Nb+2) = Pd(Nb:Nb+1);
     %blocking
-    L11=ALL(1:Nw+Nb-2,1:Nw+Nb-2);
-    L12=ALL(1:Nw+Nb-2,Nw+Nb-1:Nw+Nb+2);
-    L21=ALL(Nw+Nb-1:Nw+Nb+2,1:Nw+Nb-2);
-    L22=ALL(Nw+Nb-1:Nw+Nb+2,Nw+Nb-1:Nw+Nb+2);
+    L11 = U(1:Nw+Nb-2,            1:Nw+Nb-2);
+    L12 = U(1:Nw+Nb-2,      Nw+Nb-1:Nw+Nb+2);
+    L21 = U(Nw+Nb-1:Nw+Nb+2,      1:Nw+Nb-2);
+    L22 = U(Nw+Nb-1:Nw+Nb+2,Nw+Nb-1:Nw+Nb+2);
 
     L = L11 - L12 * (L22 \ L21);
     [v,k2] = eig(L);
 
-    v2 = -(L22 \ L21) * v;
+    v2 = - (L22 \ L21) * v;
 
-    eigvector_w = [v(1:Nw-1,:)     ; v2(1:2,:)];
-    eigvector_b = [v(Nw:Nw+Nb-2,:) ; v2(3:4,:)];
+    eigvectorw = [v(1 :Nw-1,:)    ; v2(1:2,:)];
+    eigvectorb = [v(Nw:Nw+Nb-2,:) ; v2(3:4,:)];
 
-    k2=sqrt(diag(k2));
-    [~,ind]=sort(real(k2),'descend');
-    kr=k2(ind);
+    k2      = sqrt(diag(k2));
+    [~,ind] = sort(real(k2),'descend');
+    kr      = k2(ind);
 
-    eigvector_w = eigvector_w(:,ind);
-    eigvector_b = eigvector_b(:,ind);
+    eigvectorw = eigvectorw(:,ind);
+    eigvectorb = eigvectorb(:,ind);
 
 end
 
-function [n_modes,kr,eigvector_w,eigvector_b]= Modes_number...
-    (freq,kr,eigvector_w,eigvector_b,cpmax)
+function [nmodes,kr,eigvectorw,eigvectorb] = NumofModes...
+    (w,kr,eigvectorw,eigvectorb,cpmax)
 
-    cp = 2 * pi * freq ./ real(kr);
-    n_modes  = 0;
+    cp = w / real(kr);
+    nmodes  = 0;
     for i=1:length(kr)
         if(cp(i) <= cpmax)
-            n_modes = i;
+            nmodes = i;
         end
     end
 
-    if(n_modes == 0)
+    if(nmodes == 0)
         error('Incorrect maximum phase speed input!');
     end
 
-    kr = kr(1:n_modes);
-    eigvector_w = eigvector_w(:,1:n_modes);
-    eigvector_b = eigvector_b(:,1:n_modes);
+    kr = kr(1:nmodes);
+    eigvectorw = eigvectorw(:,1:nmodes);
+    eigvectorb = eigvectorb(:,1:nmodes);
 
 end
 
-function a = Normalization_2layers(eigvector_w,eigvector_b,n_modes,...
+function a = Normalization(eigvectorw,eigvectorb,nmodes,...
     rhow,rhob,interface,bottom)
 
-    Nw = size(eigvector_w,1)-1;
-    Nb = size(eigvector_b,1)-1;
+    Nw = size(eigvectorw, 1) - 1;
+    Nb = size(eigvectorb, 1) - 1;
     x1 = cos((0 : Nw) * pi / Nw)';
     x2 = cos((0 : Nb) * pi / Nb)';
-    Rw = Convolution(Cheb(1./rhow, x1));
-    Rb = Convolution(Cheb(1./rhob, x2));
+    Rw = ConvolutionMatrix(ChebTrans(1./rhow, x1));
+    Rb = ConvolutionMatrix(ChebTrans(1./rhob, x2));
 
-    a = zeros(n_modes,1);
+    a = zeros(nmodes, 1);
 
-    P1      = zeros(Nw+1,1);
-    k       = 0:2:Nw;
-    P1(k+1) = -2./(k.^2-1);
-    P2      = zeros(Nb+1,1);
-    k       = 0:2:Nb;
-    P2(k+1) = -2./(k.^2-1);
+    P1      = zeros(Nw+1, 1);
+    P2      = zeros(Nb+1, 1);
+    
+    k       = 0 : 2 : Nw;
+    P1(k+1) = -2 ./ (k.^2 - 1);    
+    k       = 0 : 2 : Nb;
+    P2(k+1) = -2 ./ (k.^2 - 1);
 
-    for j=1:n_modes
+    for j = 1 : nmodes
 
-        f1 = Convolution(eigvector_w(:,j)) * eigvector_w(:,j);
-        f2 = Convolution(eigvector_b(:,j)) * eigvector_b(:,j);
+        f1 = ConvolutionMatrix(eigvectorw(:,j)) * eigvectorw(:,j);
+        f2 = ConvolutionMatrix(eigvectorb(:,j)) * eigvectorb(:,j);
         f1 = Rw * f1;        
         f2 = Rb * f2;
 
@@ -344,72 +343,71 @@ function a = Normalization_2layers(eigvector_w,eigvector_b,n_modes,...
 
 end
 
-function [psi,z] = Generate_modes(eigvector_w,eigvector_b,n_modes,dz,...
+function [psi,z] = GenerateModes(eigvectorw,eigvectorb,nmodes,dz,...
     rhow,rhob,interface,bottom)
 
-    zt1  = 0:dz:interface;
-    zt2  = interface:dz:bottom;
+    zt1  = 0 : dz : interface;
+    zt2  = interface : dz : bottom;
 
     xt1  = -2/interface * zt1 + 1;
     xt2  = -2/(bottom-interface)*zt2+(bottom+interface)/(bottom-interface);
 
-    psi1 = invCheb(eigvector_w,xt1);
-    psi2 = invCheb(eigvector_b,xt2);
+    psi1 = InvChebTrans(eigvectorw,xt1);
+    psi2 = InvChebTrans(eigvectorb,xt2);
     psi  = [psi1(1:length(xt1)-1,:); psi2];
 
-    a    = Normalization_2layers(eigvector_w,eigvector_b,...
-           n_modes,rhow,rhob,interface,bottom);
+    a    = Normalization(eigvectorw,eigvectorb,...
+           nmodes,rhow,rhob,interface,bottom);
 
-    for j=1:n_modes
-        psi (:,j)=psi(:,j) ./ a(j);
+    for j = 1 : nmodes
+        psi(:,j) = psi(:,j) / a(j);
     end
 
-    z=0:dz:bottom;
+    z = 0 : dz : bottom;
 
 end
 
-function [tl,tl_zr] = Calculate_tl_field(n_modes,nr,r,kr,zs,rho_zs,dz,psi,zr)
+function [tl,tl_zr] = SynthesizeSoundField(nmodes,nr,r,kr,zs,rhozs,dz,psi,zr)
 
-    bessel=zeros(n_modes,nr);
-    for im=1:n_modes
-        for ir=1:nr
+    bessel = zeros(nmodes,nr);
+    for im = 1 : nmodes
+        for ir = 1 : nr
             bessel(im,ir) = besselh(0,1,r(ir)*kr(im));
         end
     end
-    s = round(zs / dz) + 1;
-
+    
+    s   = round(zs / dz) + 1;
     psi = psi * diag(psi(s,:));
-    p   = psi * bessel * 1i * pi / rho_zs;
+    p   = psi * bessel * 1i * pi / rhozs;
 
     tl    = -20 * log10(abs(p));
     tl_zr = tl(round(zr / dz) + 1,:);
 
 end
 
-function Print_wavenumbers(kr,casename)
+function ShowWavenumbers(kr,casename)
 
     disp('plot the modal wavenumbers!');
-    figure;
+    figure;title(casename);
     plot(real(kr),imag(kr),'r*');
     xlabel('Real Wavenumber (1/m)');
-    ylabel('Imaginary Wavenumber (1/m)');
-    title(casename);
+    ylabel('Imaginary Wavenumber (1/m)');  
     set(gca,'FontSize',16,'FontName','Times New Roman');
 
 end
 
-function Print_mode(psi,z)
+function ShowMode(psi,z)
 
     figure;
     mode_num = input('What mode number do you want to plot?:');
-    plot(imag(psi(:,mode_num)),z,'k--','LineWidth',1);hold on;
+    plot(imag(psi(:,mode_num)),z,'k--','LineWidth',1) ;hold on;
     plot(real(psi(:,mode_num)),z,'r-','LineWidth',0.5);
     set(gca,'YDir','reverse');ylabel( 'Depth (m)');
     set(gca,'FontSize',16,'FontName','Times New Roman');
 
 end
 
-function Print_tl_zr(r,zr,tl_zr)
+function ShowTLcurve(r,zr,tl_zr)
 
     figure;
     disp('plot the transmission loss curve at zr!');
@@ -421,7 +419,7 @@ function Print_tl_zr(r,zr,tl_zr)
 
 end
 
-function Print_tl_field(r,z,tl,tlmin,tlmax,casename,interface)
+function ShowSoundField(r,z,tl,tlmin,tlmax,casename,interface)
 
     figure;
     disp('plot the transmission loss field!');
@@ -429,13 +427,13 @@ function Print_tl_field(r,z,tl,tlmin,tlmax,casename,interface)
     plot(r,interface*ones(length(r)),'k--','Linewidth',1.5);
     caxis( [tlmin tlmax] ); colormap( flipud(jet) );
     shading flat; colorbar; view( 0, -90 );
-    xlabel( 'Range (m)'); ylabel( 'Depth (m)');
+    xlabel( 'Range (m)')  ; ylabel( 'Depth (m)');
     colorbar( 'YDir', 'Reverse' );title(casename);
     set(gca,'FontSize',16,'FontName','Times New Roman');
 
 end
 
-function Write_to_file(filename,tlmin,tlmax,r,z,tl)
+function SaveSoundField(filename,tlmin,tlmax,r,z,tl)
 
     tl_fid = fopen(filename,'w');
     fwrite(tl_fid,length(z),'int32');
