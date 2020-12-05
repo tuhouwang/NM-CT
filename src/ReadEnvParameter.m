6ba7132b4ccb336febfbd620d74b92e6f706af83
Houwang Tu <tuhouwang96@163.com>
2020-12-05 16:22:14 +0800

    增加了有关zs和zr的范围判断

--- src/ReadEnvParameter.m
+++ src/ReadEnvParameter.m
@@ -33,32 +33,36 @@ function [casename,Nw,Nb,cpmax,dr,zs,zr,rmax,freq,interface,...
         rhob      = BProfile(3,1:nb);
         alphab    = BProfile(4,1:nb);
     else
-        error('Error! h must less than H and greater than 0!');
+        error('Error! h must greater than 0 and less than H !');
     end
     
     % Check the input underwater sound profile
     if(depw(1) ~= 0.0 || depw(nw) ~= interface ||...
             depb(1) ~= interface || depb(nb) ~= bottom)
-        error('Error! input sound profile is unsuitable!');
+        error('Error! input sound profile is unsuitable !');
     end
 
     if((interface / dz - floor(interface / dz)) ~=0 ||...
             (bottom / dz - floor(bottom / dz)) ~=0)
-        error('Error! The input dz unsuitable!');
+        error('Error! The input dz unsuitable !');
     end
 
     if((rmax / dr - floor(rmax / dr)) ~= 0)
-        error('Please reinput the dr and rmax!');
+        error('Please reinput the dr and rmax !');
     end
 
     if(Lowerboundary ~= 0 && Lowerboundary ~= 1)
-        disp('Error! The lower boundary must be rigid or soft!');
-        error('Please input 0 or 1 in Lowerboundary!');
+        disp('Error! The lower boundary must be rigid or soft !');
+        error('Please input 0 or 1 in Lowerboundary !');
     end
 
-    if(tlmin >= tlmax)
-        error('tlmin must less than tlmax!');
+    if(zs <= 0  || zs >= bottom || zr <= 0 || zr >= bottom)
+        error('zs and zr must be greater than 0 and less than H !');
     end
+    
+    if(tlmin >= tlmax)
+        error('tlmin must less than tlmax !');
+    end    
 
     fclose(fid);
 
