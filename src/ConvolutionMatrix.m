7d67d4127da937891481e28dd605c4ee67ee08e8
wangyx <yxwang@nudt.edu.cn>
2020-12-07 23:46:16 +0800

    12月7日碰头核对版

--- src/ConvolutionMatrix.m
+++ src/ConvolutionMatrix.m
@@ -8,22 +8,22 @@ function C  = ConvolutionMatrix(v)
             
             j = k - i + 1;       
             if (j >= 1 && j <= n)
-                C(k,i) = C(k,i) + v(j);
+                C(k, i) = C(k, i) + v(j);
             end
             
             j = i - k + 1;
             if (j <= n && j >= 1)
-                C(k,i) = C(k,i) + v(j);
+                C(k, i) = C(k, i) + v(j);
             end
             
             if (k > 1)
                 j = i + k - 1 ;
                 if (j <= n && j >= 1)
-                    C(k,i) = C(k,i) + v(j);
+                    C(k, i) = C(k, i) + v(j);
                 end  
             end
             
-            C(k,i) = C(k,i) * 0.5; 
+            C(k, i) = C(k, i) * 0.5; 
         end
     end
 
